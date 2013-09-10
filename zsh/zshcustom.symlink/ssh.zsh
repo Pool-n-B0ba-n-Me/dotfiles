0cf3aa172f6f6501f47e737761d861ac839b18eb
Ruoyu Li <lllrrrryyy@gmail.com>
2013-09-09 20:39:07 -0400

    zsh ssh: add aliases for x-forwarding

--- zsh/zshcustom.symlink/ssh.zsh
+++ zsh/zshcustom.symlink/ssh.zsh
@@ -1,7 +1,11 @@
 # short cut to ssh into remote hosts
 
-alias unix="ssh ruoyul@unix.andrew.cmu.edu"
+alias afs="ssh ruoyul@unix.andrew.cmu.edu"
+alias afsx="ssh -X ruoyul@unix.andrew.cmu.edu"
 alias shark="ssh ruoyul@shark.ics.cs.cmu.edu"
+alias sharkx="ssh -X ruoyul@shark.ics.cs.cmu.edu"
 
-ece()   { ssh ruoyul@ece0"$@".ece.cmu.edu }
-ghc()   { ssh ruoyul@ghc"$@".ghc.andrew.cmu.edu }
+ece()       { ssh ruoyul@ece0"$@".ece.cmu.edu }
+ecex()      { ssh -X ruoyul@ece0"$@".ece.cmu.edu }
+ghc()       { ssh ruoyul@ghc"$@".ghc.andrew.cmu.edu }
+ghcx()      { ssh -X ruoyul@ghc"$@".ghc.andrew.cmu.edu }
